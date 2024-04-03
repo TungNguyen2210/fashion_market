4dc24efce4e97bc4234510d21b2576fffd7d7738
Thanhtungdev <nguyenthanhtung221003@gmail.com>
2024-04-03 18:48:07 +0700

    update code

--- Service-Thuy/app/routers/supplier.js
+++ Service-Thuy/app/routers/supplier.js
@@ -2,11 +2,11 @@ const supplierController = require("../controllers/supplierController");
 const router = require("express").Router();
 const middleware = require('../utils/middleware');
 
-router.get('/searchByName', middleware.checkLogin, supplierController.searchSupplierByName);
-router.post('/create', middleware.checkLogin, supplierController.createSupplier);
+router.get('/searchByName', supplierController.searchSupplierByName);
+router.post('/create', supplierController.createSupplier);
 router.get('/', supplierController.getAllSuppliers);
 router.get('/:id', supplierController.getSupplierById);
-router.put('/:id', middleware.checkLogin, supplierController.updateSupplier);
-router.delete('/:id', middleware.checkLogin, supplierController.deleteSupplier);
+router.put('/:id', supplierController.updateSupplier);
+router.delete('/:id', supplierController.deleteSupplier);
 
 module.exports = router;
