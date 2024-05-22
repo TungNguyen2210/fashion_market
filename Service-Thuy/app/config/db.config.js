7e84200610fb4d39e19b7c6c0a8931736728460a
vovanhung-dev <67744712+vovanhung-dev@users.noreply.github.com>
2024-05-22 19:03:27 +0700

    update code

--- Service-Thuy/app/config/db.config.js
+++ Service-Thuy/app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    URL: process.env.MONGODB_URI || 'mongodb://vovanhung77h12:test1234@ac-kr9viol-shard-00-00.cmqyjym.mongodb.net:27017/stussy,ac-kr9viol-shard-00-01.cmqyjym.mongodb.net:27017,ac-kr9viol-shard-00-02.cmqyjym.mongodb.net:27017/?ssl=true&replicaSet=atlas-n13vet-shard-0&authSource=admin&retryWrites=true&w=majority'
+    URL: process.env.MONGODB_URI || 'mongodb://vovanhung77h12:test1234@ac-kr9viol-shard-00-00.cmqyjym.mongodb.net:27017,ac-kr9viol-shard-00-01.cmqyjym.mongodb.net:27017,ac-kr9viol-shard-00-02.cmqyjym.mongodb.net:27017/stussy?ssl=true&replicaSet=atlas-n13vet-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
